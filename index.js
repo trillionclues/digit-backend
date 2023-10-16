53905d5d1bcb39031c52c11e680a4f776394ae60
Trillionclues <trillionclues@gmail.com>
2023-10-16 22:02:40 +0100

    [fix]: cors error

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const enquiryRouter = require("./routes/enqRoute");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
@@ -22,6 +23,7 @@ app.use(morgan("dev")); // output dev timestamps in terminal
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // Root route handler
 app.get("/", (req, res) => {
